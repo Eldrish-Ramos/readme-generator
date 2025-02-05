420e4588662ac3b2053006c7ee8e69dcd7dc1c41
Eldrish <eldrish.ramosperez@gmail.com>
2025-02-05 14:01:27 -0800

    updated package.json with module type, installed required packages, added sample code to test running code in termal with node

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import colors from 'colors';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+[
+    {
+        name: 'firstInput',
+        type: 'input',
+        message: colors.brightMagenta(`My Message Here`)
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() 
+{
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
