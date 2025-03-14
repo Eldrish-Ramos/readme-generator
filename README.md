3adb7c6ec9dcff3b1985e395f8d446b0be2f4678
Eldrish <eldrish.ramosperez@gmail.com>
2025-03-13 17:56:51 -0700

    project working, added a gitignore

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-{"projectTitle":"asdasd","projectDesc":"asdssdfdsfdsfsd"}
+# Project Title
+
+## Description
+undefined
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+npm install
+
+## Usage
+npm start
+
+
+## License
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+## Contributing
+Contributions are welcome!
+
+## Tests
+npm test
+
+## Questions
+If you have any questions about the repo, open an issue or contact [Eldrish-Ramos](https://github.com/Eldrish-Ramos) directly at Eldrish.ramosperez@gmail.com.
--- index.js
+++ index.js
@@ -2,52 +2,108 @@
 import inquirer from 'inquirer';
 import colors from 'colors';
 import fs from 'fs';
-
 import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = 
-[
+const questions = [
     {
-        name: 'projectTitle',
+        name: 'title',
         type: 'input',
-        message: colors.bgBrightGreen(`Please enter a title for your project: `),
+        message: colors.bgBrightGreen('Please enter a title for your project: '),
         default: 'Project Title',
-        validate(projectTitle)
-        {
-            return projectTitle.trim().length > 0 ? true : "Please provide a valid project title!";
+        validate(projectTitle) {
+            return projectTitle.trim().length > 0 ? true : 'Please provide a valid project title!';
         }
     },
     {
         name: 'projectDesc',
         type: 'input',
-        message: colors.bgYellow(`Please enter a description for your project: `),
+        message: colors.bgYellow('Please enter a description for your project: '),
         default: 'No Description Provided',
-        validate(projectDesc)
-        {
-            return projectDesc.trim().length > 0 ? true : "Please provide a valid description for your project!";
+        validate(projectDesc) {
+            return projectDesc.trim().length > 0 ? true : 'Please provide a valid description for your project!';
+        }
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Please provide installation instructions: ',
+        default: 'npm install',
+        validate(installation) {
+            return installation.trim().length > 0 ? true : 'Please provide valid installation instructions!';
+        }
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Please provide usage information: ',
+        default: 'npm start',
+        validate(usage) {
+            return usage.trim().length > 0 ? true : 'Please provide valid usage information!';
+        }
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Please choose a license for your project: ',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+        default: 'MIT'
+    },
+    {
+        name: 'contributing',
+        type: 'input',
+        message: 'Please provide contribution guidelines: ',
+        default: 'Contributions are welcome!',
+        validate(contributing) {
+            return contributing.trim().length > 0 ? true : 'Please provide valid contribution guidelines!';
+        }
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Please provide test instructions: ',
+        default: 'npm test',
+        validate(tests) {
+            return tests.trim().length > 0 ? true : 'Please provide valid test instructions!';
+        }
+    },
+    {
+        name: 'github',
+        type: 'input',
+        message: 'Please enter your GitHub username: ',
+        validate(github) {
+            return github.trim().length > 0 ? true : 'Please provide a valid GitHub username!';
+        }
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Please enter your email address: ',
+        validate(email) {
+            return email.trim().length > 0 ? true : 'Please provide a valid email address!';
         }
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) 
-{
-    fs.writeFile(fileName, JSON.stringify(data), (err) =>{
-
-        if (err) throw err
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('README.md has been generated!');
     });
-};
+}
 
 // TODO: Create a function to initialize app
-function init() 
-{
-    return inquirer.prompt(questions)
+function init() {
+    inquirer.prompt(questions)
         .then(responses => {
-            const genMarkdown = generateMarkdown(questions)
-            writeToFile('README.md', responses)
+            const markdown = generateMarkdown(responses);
+            writeToFile('README.md', markdown);
         })
-};
+        .catch(error => {
+            console.error('Error generating README:', error);
+        });
+}
 
 // Function call to initialize app
 init();
